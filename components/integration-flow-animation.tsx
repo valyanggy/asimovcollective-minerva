31ec3e0796f28c145ab0a17a19ed67b01c2dbe71
Val from Asimov <valfromasimov@Vals-MacBook-Pro.local>
2026-06-05 15:01:24 -0400

    Fix integration particle direction

--- components/integration-flow-animation.tsx
+++ components/integration-flow-animation.tsx
@@ -37,18 +37,28 @@ const pathDarkPaths = [
   "M236 148.291H350",
 ] as const
 
+const outputParticlePaths = [
+  "M236 149.291C236 232.134 287.04 299.291 350 299.291",
+  "M236 149.291C236 67.001 287.04 0.291444 350 0.291445",
+  "M236 148.291C236 202.968 287.04 247.291 350 247.291",
+  "M236 148.291C236 94.1675 287.04 50.2914 350 50.2914",
+  "M236 149.291C236 176.906 287.04 199.291 350 199.291",
+  "M236 149.291C236 121.125 287.04 98.2914 350 98.2914",
+  "M236 148.291H350",
+] as const
+
 const particles = [
   { path: pathDarkPaths[7], duration: "4.4s", delay: "-0.2s" },
   { path: pathDarkPaths[7], duration: "5.6s", delay: "-2.1s" },
   { path: pathDarkPaths[5], duration: "4.9s", delay: "-1.4s" },
   { path: pathDarkPaths[5], duration: "6.2s", delay: "-3.5s" },
-  { path: pathDarkPaths[1], duration: "4.9s", delay: "-0.8s" },
-  { path: pathDarkPaths[3], duration: "5.5s", delay: "-1.6s" },
-  { path: pathDarkPaths[6], duration: "4.7s", delay: "-2.4s" },
-  { path: pathDarkPaths[8], duration: "5.2s", delay: "-3.1s" },
-  { path: pathDarkPaths[4], duration: "4.8s", delay: "-1.1s" },
-  { path: pathDarkPaths[2], duration: "5.8s", delay: "-2.9s" },
-  { path: pathDarkPaths[0], duration: "5.1s", delay: "-3.7s" },
+  { path: outputParticlePaths[1], duration: "4.9s", delay: "-0.8s" },
+  { path: outputParticlePaths[3], duration: "5.5s", delay: "-1.6s" },
+  { path: outputParticlePaths[5], duration: "4.7s", delay: "-2.4s" },
+  { path: outputParticlePaths[6], duration: "5.2s", delay: "-3.1s" },
+  { path: outputParticlePaths[4], duration: "4.8s", delay: "-1.1s" },
+  { path: outputParticlePaths[2], duration: "5.8s", delay: "-2.9s" },
+  { path: outputParticlePaths[0], duration: "5.1s", delay: "-3.7s" },
 ] as const
 
 const dotColors = ["#E5B74D", "#2D3CAE", "#2D3CAE", "#11196F", "#E5B74D", "#2D3CAE", "#11196F"]
